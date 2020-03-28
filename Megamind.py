0958159e019e677d0a1269aa531e2312d23e7caa
Schwarcz Florian <florian.schwarcz@gmx.at>
2020-03-28 21:45:46 +0100

    Added basic Megamind

---
+++ Megamind.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from typing import Dict, List
+
+from Game import Game, State
+from MTTTBot import MTTTBot
+
+
+class Megamind(MTTTBot):
+
+    def __init__(self, game: Game) -> None:
+        super().__init__(game)
+
+        self.mind: Dict[State, np.ndarray] = {}
+        self.decisions: Dict[State, tuple[int, int, int, int]] = List[]
+
+    def play(self):
+        if self.game.game_over():
+            return
+        if self.game.state not in self.mind:
+            self.mind[self.game.state] = np.zeros((3, 3, 3, 3))
+        while True:
+            # calc probs and pick position
+            position = (0, 0, 0, 0)
+            if self.game.play(position):
+                self.decisions[self.game.state] = position
+                break
+            self.mind[self.game.state][position] = None
+
+    def game_over(self, won: bool) -> None:
+        for state, position in self.decisions.items():
+            # reward or punish
+            pass
